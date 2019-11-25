d200a6cc1736eeeed71f4720747c5597304d10c6
jaewonYoon <21300492@handong.edu>
2019-11-25 18:49:19 +0900

    hooks webpack config done

--- hooks_example/webpack.config.js
+++ hooks_example/webpack.config.js
@@ -1,13 +1,37 @@
+const webpack = require('webpack'); 
+
 module.exports = {
     entry: [
       './src/index.js'
     ],
+    module: {
+        rules: [
+            {
+            test: /\.m?(js|jsx)$/,
+            resolve: {
+                extensions: ['*',".js", ".jsx"]
+            },
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/preset-env']
+                }
+            }
+            }
+        ]
+    },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
       filename: 'bundle.js'
     },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ], 
     devServer: {
-      contentBase: './public/'
+      contentBase: './src/',
+      hot: true
     }
   };
+
